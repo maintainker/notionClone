59a4941b94b3a9cb354e0d4d9b678bd2995e1092
설영환 <yhsul@seol-yeonghwan-ui-MacBookPro.local>
2020-09-18 08:30:37 +0900

    ref로 box조절실패

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useCallback, useRef } from 'react';
 import NoteLine from './components/noteLine'
+import LineList from './components/lineList';
 
 const App = () =>{
   const [notes,setNotes] = useState([]); // 노트를 배열로 저장
@@ -7,7 +8,7 @@ const App = () =>{
     event:"none",
     target:null
   })
-  
+  const searchList = useRef(""); 
 
   useEffect(()=>{
     if(notes.length===0){ // note의 length가 0이면 새로운 라인생성
@@ -16,13 +17,14 @@ const App = () =>{
         content:""
       }])
     }
-    // console.log(e.target.closest("section").nextSibling)
-    if(focus.current.event==="keypress Enter"){
+    if(focus.current.event==="keypress Enter"){ // 엔터시에 포커스 이동하기
       focus.current.target.nextSibling.firstChild.focus();
-      focus.current={...focus.current
-        ,target:focus.current.target.nextSibling
-        ,event:"none"}
+      focus.current={...focus.current,
+        target:focus.current.target.nextSibling,
+        event:"none"
+      }
     }
+   
   },[notes])
   
   const createLine = useCallback((e, index) =>{//엔터 누를때 다음줄에 contents생성
@@ -39,6 +41,14 @@ const App = () =>{
       }
       setNotes(newContent);
       
+    }else if(e.key && ((e.type === "keypress" && e.key ==="/")||searchList.current!=="")){
+      // console.log(window.getSelection())
+      searchList.current = searchList.current+e.key;
+      console.log(searchList.current)
+      const newContent =[...notes];
+      newContent[index].content += e.key;
+      console.log(newContent)
+      // setNotes(newContent)
     }else if(e.type ==="blur"){ // blur 되도 컨텐츠 저장하게
       const newContent =[...notes];
       newContent[index].content =e.target.innerText;
@@ -53,8 +63,8 @@ const App = () =>{
 
   return (
     <section id="notePad">
-
       {noteLines}
+      {searchList.current!=="" && <LineList/>}
     </section>
   );
 }
---
+++ src/components/lineList/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./test.css";
+
+const LineList = () =>{
+    return (<div className="box"></div>)
+}
+
+export default LineList;
